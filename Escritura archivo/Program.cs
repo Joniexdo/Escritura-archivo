72e12a958c6ceab857667ecd3bae770eb0b5a8d4
Joniexdo <105814952+Joniexdo@users.noreply.github.com>
2022-06-03 14:04:18 -0700

    Version tarea

--- Escritura archivo/Program.cs
+++ Escritura archivo/Program.cs
@@ -9,15 +9,38 @@ namespace Escritura_archivo
 {
     class Program
     {
+        class Persona
+        {
+            public string nombre;
+            public byte edad;
+            public Persona(string nombre, byte edad)
+            {
+                this.nombre = nombre;
+                this.edad = edad;
+            }
+        }
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
-            Console.Write("Ingrese su nombre: ");
-            string nombre = Console.ReadLine();
-            sw.Write(nombre);
-            sw.Close();
-            Console.WriteLine("Escribiendo");
-            Console.ReadLine();
+            try
+            {
+                StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+                Console.Write("Ingrese su nombre: ");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese su edad: ");
+                byte edad = byte.Parse(Console.ReadLine());
+                Persona alm = new Persona(nombre, edad);
+
+                sw.WriteLine(alm.nombre + "" + alm.edad);
+
+                sw.Close();
+                Console.WriteLine("Escribiendo");
+                Console.ReadLine();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Datos erronenos");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
